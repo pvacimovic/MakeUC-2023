ea103a91ba3b9b7477f9d4b38ea5c477f4310f49
Anagh19 <anagh.mishra07191@gmail.com>
2023-11-04 18:23:17 -0400

    change clock type

--- clocksys.py
+++ clocksys.py
@@ -1,16 +1,22 @@
 import time
 from time import sleep
 import pwm
+from datetime import datetime
+import pytz
 
 targetHour = int(input("Enter the Hour when you want to wake up (24hr): ")) #5 pm
 targetMinute = int(input("Enter the Minute when you want to wake up: ")) #5 pm
 activated = 0
-print(f'Target Hour - Target Minute: {targetHour} - {targetMinute} ')
+
+target_timezone = pytz.timezone('US/Eastern')#('America/New_York')  # Replace with your desired timezone
+datetime.now(target_timezone)
+
+print(f'Target Hour - Target Minute: {targetHour}:{targetMinute} ')
 while(True):
-    t = time.localtime(time.time())
-    print(f'Time:{int(t.tm_hour)%12}:{t.tm_min}')
+    t = datetime.now()
+    print(f'Time:{int(t.hour)}:{t.minute}')
     sleep(.5)
-    if((t.tm_hour == targetHour) and (targetMinute -1 == t.tm_min) and not activated):
+    if((t.hour == targetHour) and (targetMinute -1 == t.minute) and not activated):
         activated = 1
         print("LIGHTS TURNING ON")
         pwm.lightingUp()
