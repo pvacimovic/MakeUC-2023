46647c14df3eaf3a02d9a85650ab5ad8d37c5617
Anagh19 <anagh.mishra07191@gmail.com>
2023-11-04 17:51:22 -0400

    integrating

--- clocksys.py
+++ clocksys.py
@@ -6,12 +6,12 @@ targetHour = int(input("Enter the Hour when you want to wake up (24hr): ")) #5 p
 targetMinute = int(input("Enter the Minute when you want to wake up: ")) #5 pm
 activated = 0
 
-# while(True):
-#     t = time.localtime(time.time())
-#     print(t)
-#     if((t.tm_hour == targetHour) and (targetMinute -1 == t.tm_min) and not activated):
-#         activated = 1
-#         print("LIGHTS TURNING ON")
-#         pwm.lightingUp()
+while(True):
+    t = time.localtime(time.time())
+    print(t)
+    if((t.tm_hour == targetHour) and (targetMinute -1 == t.tm_min) and not activated):
+        activated = 1
+        print("LIGHTS TURNING ON")
+        pwm.lightingUp()
     
     
