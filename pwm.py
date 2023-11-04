383826b4be9a4b79820b5b9ea8a7363d1b41bfd1
Anagh19 <anagh.mishra07191@gmail.com>
2023-11-04 17:48:12 -0400

    adding main initializer

--- pwm.py
+++ pwm.py
@@ -3,15 +3,20 @@ import datetime
 import time
 from time import sleep
 
-led = 33
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(led, GPIO.OUT)
-ledFade = GPIO.PWM(led, 1000)
-ledFade.start(0)
+def lightingUp():
+    led = 33
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(led, GPIO.OUT)
+    ledFade = GPIO.PWM(led, 1000)
+    ledFade.start(0)
 
-while True:
-    t = time.localtime(time.time())
-    dutyVal = (5/3)*(t.tm_sec)
-    if (dutyVal%1 == 0): print(dutyVal)
-    ledFade.ChangeDutyCycle(dutyVal)
-    sleep(0.5)
+    while True:
+        t = time.localtime(time.time())
+        dutyVal = (5/3)*(t.tm_sec)
+        if (dutyVal%1 == 0): print(dutyVal)
+        ledFade.ChangeDutyCycle(dutyVal)
+        sleep(0.5)
+if __name__ == '__main__':
+    # test1.py executed as script
+    # do something
+    lightingUp()
