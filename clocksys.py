0a3df0f91e99fac8944c493787a252121125bfca
Anagh19 <anagh.mishra07191@gmail.com>
2023-11-04 17:17:24 -0400

    getting input for alarm time

---
+++ clocksys.py
@@ -0,0 +1,16 @@
+import time
+from time import sleep
+import pwm
+
+targetHour = int(input("Enter the Hour when you want to wake up (24hr): ")) #5 pm
+targetMinute = int(input("Enter the Minute when you want to wake up: ")) #5 pm
+activated = 0
+
+while(True):
+    t = time.localtime(time.time())
+    print(t)
+    if((t.tm_hour == targetHour) and (targetMinute -1 == t.tm_min) and not activated):
+        activated = 1
+        pwm.lightingUp()
+    
+    
