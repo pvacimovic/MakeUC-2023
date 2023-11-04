c34cd3e57c93e13a4ac44196d99214949dff0ee9
Anagh19 <anagh.mishra07191@gmail.com>
2023-11-04 17:56:27 -0400

    reducing prints

--- clocksys.py
+++ clocksys.py
@@ -5,10 +5,11 @@ import pwm
 targetHour = int(input("Enter the Hour when you want to wake up (24hr): ")) #5 pm
 targetMinute = int(input("Enter the Minute when you want to wake up: ")) #5 pm
 activated = 0
-
+print(targetHour + " "+targetMinute)
 while(True):
     t = time.localtime(time.time())
-    print(t)
+    print(f'Time:{int(t.tm_hour)%12}:{t.tm_min}')
+    sleep(.5)
     if((t.tm_hour == targetHour) and (targetMinute -1 == t.tm_min) and not activated):
         activated = 1
         print("LIGHTS TURNING ON")
