1e0c7782cb96f514f5d483b6aaa716d8cbe81e9f
Anagh19 <anagh.mishra07191@gmail.com>
2023-11-04 15:22:41 -0400

    Added pwm

---
+++ pwm.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+led = 33
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(base, GPIO.OUT)
+ledFade = GPIO.PWM(led, 1000)
+ledFade.start(0)
+
+while True:
+    for i in range(0,101, 5):
+        ledFade.ChangeDutyCycle(i)
+        sleep(0.05)
