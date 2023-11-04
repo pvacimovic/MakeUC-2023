7fc6f7a325296a3646c20d1201a2c78bdaf93bbf
Anagh19 <anagh.mishra07191@gmail.com>
2023-11-04 17:58:53 -0400

    syntaxError

--- clocksys.py
+++ clocksys.py
@@ -5,7 +5,7 @@ import pwm
 targetHour = int(input("Enter the Hour when you want to wake up (24hr): ")) #5 pm
 targetMinute = int(input("Enter the Minute when you want to wake up: ")) #5 pm
 activated = 0
-print(targetHour + " "+targetMinute)
+print(f'Target Hour - Target Minute: {targetHour} - {targetMinute} ')
 while(True):
     t = time.localtime(time.time())
     print(f'Time:{int(t.tm_hour)%12}:{t.tm_min}')
